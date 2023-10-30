d81a7a3183078eaad7a76e91b83a927d6c0cc857
Evgeniy K <pgg1337@mail.ru>
2023-10-30 17:12:00 +0200

    add app accounts and models

---
+++ accounts/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='user_images/', default='user_images/default/default-avatar-profile.jpg')
+    description = models.TextField()
+
+
+
